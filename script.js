1631ad682c04182252ba01e50e874dc652878777
minkman <lomick2090@gmail.com>
2022-12-01 11:17:55 -0600

    Add win coniditions

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 const squares = document.querySelectorAll('.square');
-let gameboard = [];
+let gameBoard = [];
+let turn = 1;
 
 
-const game = (() => {
+const gameControls = (() => {
 
-    let turn = 1;
+    const checkForWin = () => {
+        if ((gameBoard[0] == 1 && gameBoard[1] == 1 && gameBoard[2] == 1) ||
+            (gameBoard[3] == 1 && gameBoard[4] == 1 && gameBoard[5] == 1) ||
+            (gameBoard[6] == 1 && gameBoard[7] == 1 && gameBoard[8] == 1) ||
+            (gameBoard[0] == 1 && gameBoard[3] == 1 && gameBoard[6] == 1) ||
+            (gameBoard[1] == 1 && gameBoard[4] == 1 && gameBoard[7] == 1) ||
+            (gameBoard[2] == 1 && gameBoard[5] == 1 && gameBoard[8] == 1) ||
+            (gameBoard[0] == 1 && gameBoard[3] == 1 && gameBoard[8] == 1) ||
+            (gameBoard[6] == 1 && gameBoard[4] == 1 && gameBoard[2] == 1)) {
+                //x's win
+                console.log('x wins!')
+            } else if((gameBoard[0] == 2 && gameBoard[1] == 2 && gameBoard[2] == 2) ||
+            (gameBoard[3] == 2 && gameBoard[4] == 2 && gameBoard[5] == 2) ||
+            (gameBoard[6] == 2 && gameBoard[7] == 2 && gameBoard[8] == 2) ||
+            (gameBoard[0] == 2 && gameBoard[3] == 2 && gameBoard[6] == 2) ||
+            (gameBoard[1] == 2 && gameBoard[4] == 2 && gameBoard[7] == 2) ||
+            (gameBoard[2] == 2 && gameBoard[5] == 2 && gameBoard[8] == 2) ||
+            (gameBoard[0] == 2 && gameBoard[4] == 2 && gameBoard[8] == 2) ||
+            (gameBoard[6] == 2 && gameBoard[4] == 2 && gameBoard[2] == 2)) {
+                //o's win
+                console.log('o wins!')
+            }
+    }
+
+    const updateGameBoard = (square, move) => {
+        gameBoard[square] = move;
+        checkForWin();
+    }
 
     function makeMove() {
 
@@ -12,10 +40,12 @@ const game = (() => {
             if (!(turn % 2 == 0)) {
                 this.innerHTML = 'X';
                 turn++
+                updateGameBoard(this.data , 1)
 
             } else {
                 this.innerHTML = 'O';
                 turn++
+                updateGameBoard(this.data , 2)
             }
         };
     
@@ -28,8 +58,7 @@ const game = (() => {
 
 })();
 
-
-
-squares.forEach((element) => {
-    element.addEventListener('click', game.makeMove)
-});
+for (i=0; i < squares.length; i ++) {
+    squares[i].addEventListener('click', gameControls.makeMove);
+    squares[i].data = i;
+}
