b9f63a6c1ae5671ab3a9fe5d6f1694efa43c1669
minkman <lomick2090@gmail.com>
2022-12-04 11:52:36 -0600

    Add naming functionality

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const squares = document.querySelectorAll('.square');
+const nameSubmits = document.querySelectorAll('.namesubmit');
+const playerName = document.querySelectorAll('div>h1')
 
-let gameState = {
-    gameBoard: [],
-    turn: 1
-}
 
 const gameControls = (() => {
+
+    let gameState = {
+        player1: {name: 'PLAYER 1',
+                  wins: 0},
+        player2: {name: 'PLAYER 2',
+                  wins: 0},
+        gameBoard: [],
+        turn: 1,
+        gameComplete: false,
+    }
 
     const checkForWin = () => {
         if ((gameState.gameBoard[0] == 1 && gameState.gameBoard[1] == 1 && gameState.gameBoard[2] == 1) ||
@@ -18,6 +26,7 @@ const gameControls = (() => {
             (gameState.gameBoard[6] == 1 && gameState.gameBoard[4] == 1 && gameState.gameBoard[2] == 1)) {
                 //x's win
                 console.log('x wins!')
+                gameState.gameComplete = true;
             } else if((gameState.gameBoard[0] == 2 && gameState.gameBoard[1] == 2 && gameState.gameBoard[2] == 2) ||
             (gameState.gameBoard[3] == 2 && gameState.gameBoard[4] == 2 && gameState.gameBoard[5] == 2) ||
             (gameState.gameBoard[6] == 2 && gameState.gameBoard[7] == 2 && gameState.gameBoard[8] == 2) ||
@@ -28,9 +37,11 @@ const gameControls = (() => {
             (gameState.gameBoard[6] == 2 && gameState.gameBoard[4] == 2 && gameState.gameBoard[2] == 2)) {
                 //o's win
                 console.log('o wins!')
+                gameState.gameComplete = true;
             } else if (gameState.gameBoard.length == 9 && !(gameState.gameBoard.includes(undefined))) {
                 //tie game
                 console.log('tie game!')
+                gameState.gameComplete = true;
             }
     }
 
@@ -39,18 +50,25 @@ const gameControls = (() => {
         checkForWin();
     }
 
+    function updatePlayerName(input, position) {
+        gameState.player1.name = input;
+        playerName[position].innerHTML = input;
+    }
+
     function makeMove() {
 
-        if (this.innerHTML == '') {
+        if (gameState.gameComplete){
+            alert('Game Over ... restart game to play!')
+        } else if (this.innerHTML == '') {
             if (!(gameState.turn % 2 == 0)) {
                 this.innerHTML = 'X';
-                gameState.turn++
-                updateGameState(this.data , 1)
+                gameState.turn++;
+                updateGameState(this.data , 1);
 
             } else {
                 this.innerHTML = 'O';
-                gameState.turn++
-                updateGameState(this.data , 2)
+                gameState.turn++;
+                updateGameState(this.data , 2);
             }
         };
     
@@ -58,12 +76,19 @@ const gameControls = (() => {
     };
 
     return {
-        makeMove
+        makeMove,
+        updatePlayerName
     }
 
 })();
 
+
+nameSubmits.forEach((button) => button.addEventListener('click', function() { 
+    gameControls.updatePlayerName(button.previousElementSibling.value, Array.prototype.indexOf.call(nameSubmits, button));
+    button.parentNode.remove();
+    }));
+
 for (i=0; i < squares.length; i ++) {
     squares[i].addEventListener('click', gameControls.makeMove);
     squares[i].data = i;
-}
+};
